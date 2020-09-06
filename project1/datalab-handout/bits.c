d6b6551dddf9e1bec427d08e7229c5fdfca9a0ed
Andy <qingjielu990916@gmail.com>
2020-09-06 18:00:02 -0400

    negate

--- project1/datalab-handout/bits.c
+++ project1/datalab-handout/bits.c
@@ -196,7 +196,7 @@ int isTmax(int x) {
  *   Rating: 2
  */
 int negate(int x) {
-  return 2;
+  return ~x+1;
 }
 /*
  * isAsciiDigit - return 1 if 0x30 <= x <= 0x39 (ASCII codes for characters '0' to '9')
